0828e6324ecad828a611eeeee71c726ad6fe4a5e
SimoneDUrso <simone.durso98@gmail.com>
2024-10-31 16:00:04 +0100

    [UPD] Filled restaurant migration and dishes migration with seeders

--- database/seeders/DishSeeder.php
+++ database/seeders/DishSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Dish;
+use Illuminate\Support\Str;
+
 class DishSeeder extends Seeder
 {
     /**
@@ -14,6 +16,19 @@ class DishSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $dishes = config('db_deliveboo.dishes');
+        foreach ($dishes as $dish) {
+            $new_dish = new dish();
+            $new_dish->name = $dish['name'];
+            $new_dish->slug = Str::slug($dish['name'], '-');
+            $new_dish->course = $dish['course'];
+            $new_dish->description = $dish['description'];
+            $new_dish->price = $dish['price'];
+            $new_dish->vegetarian = $dish['vegetarian'];
+            $new_dish->visible = $dish['visible'];
+            $new_dish->restaurant_id = $dish['restaurant_id'];
+
+            $new_dish->save();
+        }
     }
 }
