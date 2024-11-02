114193022cba1955b222c590d384472b8ef16132
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-11-02 17:23:22 +0100

    [UPD] Edit rrestaurant done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
             <h1 class="display-4 font-weight-bold mb-4">Benvenuti su DeliveBoo!</h1>
             <p class="lead mb-5">Ordina i tuoi piatti preferiti dai migliori ristoranti della città, consegnati direttamente
                 a casa tua.</p>
-            <a href="{{ route('restaurants.index') }}" class="btn btn-primary btn-lg">Scopri i Ristoranti</a>
+            <a href="{{ route('admin.restaurants.index') }}" class="btn btn-primary btn-lg">Scopri i Ristoranti</a>
         </div>
     </div>
 @endsection
