87d677b4f3e5fc0ba4340e64a6a0e516084eb5be
AleFarinaIV <Alessandrofarina401@gmail.com>
2024-11-16 17:15:00 +0100

    [UPD] setup seeder function

--- database/seeders/TypesRestaurantsSeeder.php
+++ database/seeders/TypesRestaurantsSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class TypesRestaurantsSeeder extends Seeder
 {
@@ -14,6 +15,27 @@ class TypesRestaurantsSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // Recupera gli ID dei ristoranti e delle tipologie
+        $restaurantIds = DB::table('restaurants')->pluck('id')->toArray();
+        $typeIds = DB::table('types')->pluck('id')->toArray();
+
+        // Associa casualmente tipologie ai ristoranti
+        $associations = [];
+        foreach ($restaurantIds as $restaurantId) {
+            // Numero casuale di tipologie da associare a ogni ristorante (es. da 1 a 3)
+            $randomTypeIds = collect($typeIds)->random(rand(1, 3))->toArray();
+
+            foreach ($randomTypeIds as $typeId) {
+                $associations[] = [
+                    'restaurant_id' => $restaurantId,
+                    'type_id' => $typeId,
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
+        
+        // Inserisce le associazioni nella tabella ponte
+        DB::table('types_restaurants')->insert($associations);
     }
 }
